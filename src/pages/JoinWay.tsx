382abaab1b17c0307046f7bd5de6c76b4477a317
2paperstar <lee@paperst.ar>
2024-02-16 00:35:34 +0900

    fix: join way layout

--- src/pages/JoinWay.tsx
+++ src/pages/JoinWay.tsx
@@ -4,12 +4,12 @@ import LinkText from './component/LinkText';
 
 export default function JoinWay() {
   return (
-    <div className="mx-[10%] pt-[30%]">
+    <div className="mx-[10%] pt-[30%] flex flex-col gap-4">
       <h3 className="text-2xl leading-[250px] text-center font-bold">
         회원가입 방법을 선택하세요!
       </h3>
-      <Link to="/join">
-        <Btn className="bg-primary text-white">이메일로 가입하기</Btn>
+      <Link to="/join" className="w-full">
+        <Btn className="bg-primary text-white w-full">이메일로 가입하기</Btn>
       </Link>
       <Btn className="bg-[#F2DA00] text-[#282732] border border-[#E8E9EA]">
         카카오톡으로 가입하기
