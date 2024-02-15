ec4d014c6ca74fcfaeb35e7b36621f113582aade
2paperstar <lee@paperst.ar>
2024-02-16 06:51:05 +0900

    fix: change api url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://wayu.hackathon.sparcs.net/' });
+const api = axios.create({
+  // baseURL: 'https://wayu.hackathon.sparcs.net/'
+  baseURL: 'https://fc5c-49-165-90-85.ngrok-free.app',
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('accessToken');
