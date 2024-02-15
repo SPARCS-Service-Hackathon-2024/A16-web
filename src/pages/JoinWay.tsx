39ae1b830cc67b21c6a83c6ae67bf279847fc4e3
2paperstar <lee@paperst.ar>
2024-02-15 23:40:03 +0900

    refactor: use tailwind preset name

--- src/pages/JoinWay.tsx
+++ src/pages/JoinWay.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Btn from './component/Btn';
 import LinkText from './component/LinkText';
 
@@ -8,13 +7,12 @@ export default function JoinWay() {
       <h3 className="text-2xl leading-[250px] text-center font-bold">
         회원가입 방법을 선택하세요!
       </h3>
-      <Btn text="이메일로 가입하기" className="bg-primary text-[#ffffff]" />
-      <Btn
-        text="카카오톡으로 가입하기"
-        className="bg-[#F2DA00] text-[#282732] border border-[#E8E9EA]"
-      />
+      <Btn className="bg-primary text-white">이메일로 가입하기</Btn>
+      <Btn className="bg-[#F2DA00] text-[#282732] border border-[#E8E9EA]">
+        카카오톡으로 가입하기
+      </Btn>
       <LinkText
-        className="leading-[80px] mb-[40px] justify-center"
+        className="leading-10 mb-10 justify-center"
         text="이미 회원이신가요?"
         LinkMsg="로그인"
         to="/Login"
