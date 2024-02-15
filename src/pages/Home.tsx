77f31ec0a7552ba2ab0e2bc7e8f76b53ffa8e1fb
2paperstar <lee@paperst.ar>
2024-02-16 02:23:00 +0900

    style: home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
+import BannerSlider from './component/BannerSlider';
+import CategoryMenu from './component/CategoryMenu';
 import Header from './component/Header';
 import Menu from './component/Menu';
-import BannerSlider from './component/BannerSlider';
 import NowReviews from './component/NowReviews';
-import CategoryMenu from './component/CategoryMenu';
 
 export default function Home() {
-    return (
-        <div className='h-[100%] pt-[56px] pb-[70px] '>
-            <Header alarm={true} review={true} search={true} />
-            <div className='mx-[10%]'>
-            <BannerSlider />
-            </div>
-            <CategoryMenu />
-            <div className="bar w-full h-[8px] bg-[#f5f5f5]"></div>
-            <NowReviews />
-            <Menu home={true}/>
-        </div>
-    );
+  return (
+    <div className="h-screen">
+      <Header alarm={true} review={true} search={true} />
+      <div className="mx-[10%]">
+        <BannerSlider />
+      </div>
+      <CategoryMenu />
+      <div className="bar w-full h-[8px] bg-[#f5f5f5]"></div>
+      <NowReviews />
+      <Menu home={true} />
+    </div>
+  );
 }
-
