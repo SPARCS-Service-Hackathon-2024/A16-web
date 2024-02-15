09d2dfaf3ec6ffd84fb2b7d54d2437826f4339a4
2paperstar <lee@paperst.ar>
2024-02-15 23:54:02 +0900

    feat: move to join page

--- src/pages/JoinWay.tsx
+++ src/pages/JoinWay.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Btn from './component/Btn';
 import LinkText from './component/LinkText';
 
@@ -7,7 +8,9 @@ export default function JoinWay() {
       <h3 className="text-2xl leading-[250px] text-center font-bold">
         회원가입 방법을 선택하세요!
       </h3>
-      <Btn className="bg-primary text-white">이메일로 가입하기</Btn>
+      <Link to="/join">
+        <Btn className="bg-primary text-white">이메일로 가입하기</Btn>
+      </Link>
       <Btn className="bg-[#F2DA00] text-[#282732] border border-[#E8E9EA]">
         카카오톡으로 가입하기
       </Btn>
