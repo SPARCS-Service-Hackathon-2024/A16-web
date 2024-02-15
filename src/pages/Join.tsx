7c8ba2500b148a3d6597dd27f5757e70193bf128
2paperstar <lee@paperst.ar>
2024-02-16 00:16:35 +0900

    feat: fill error in join page

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -1,41 +1,43 @@
+import { useState } from 'react';
 import Btn from './component/Btn';
 import Header from './component/Header';
 import Input from './component/Input';
 
 export default function Join() {
+  const [emailError, setEmailError] = useState<string>();
+  const [passwordError, setPasswordError] = useState<string>();
+  const [passwordConfirmError, setPasswordConfirmError] = useState<string>();
+  const [nicknameError, setNicknameError] = useState<string>();
+
   return (
-    <div className="h-[100vh]">
+    <div className="h-screen flex flex-col">
       <Header text="회원가입" back={true} border={true} />
-      <div className="relative mx-[10%] pt-[50px] h-[100%]">
+      <div className="px-8 py-4 flex-1 flex flex-col">
         <Input
           type="text"
           text="abcde@gmail.com"
           label="이메일"
-          errorMessage="규칙에 맞는 이메일을 입력해주세요!"
-          className="mt-[10%] h-[115px]"
+          errorMessage={emailError}
         />
         <Input
           type="password"
           text="영어 대소문자, 숫자로 구성"
           label="비밀번호"
-          className="h-[115px]"
+          errorMessage={passwordError}
         />
         <Input
           type="password"
           text="비밀번호 확인"
           label="비밀번호 확인"
-          className="h-[115px]"
+          errorMessage={passwordConfirmError}
         />
         <Input
           type="text"
-          text="닉네임 입"
+          text="닉네임 입력"
           label="닉네임"
-          errorMessage="중복된 닉네임 입니다!"
-          className="h-[115px]"
+          errorMessage={nicknameError}
         />
-        <Btn className="bg-primary text-[#ffffff] absolute bottom-[5%] left-0">
-          가입하기
-        </Btn>
+        <Btn className="bg-primary text-white left-0 mt-auto">가입하기</Btn>
       </div>
     </div>
   );
